50e569d54a395178cb9d464c4b706a1b61d66c04
eliott <eliottdhommee@gmail.com>
2022-05-16 10:38:18 +0200

    small changes

--- bonus/scripts/setup-cluster.sh
+++ bonus/scripts/setup-cluster.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
+#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+#chmod 700 get_helm.sh
+#./get_helm.sh
+
 kubectl create namespace gitlab
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 helm upgrade --install gitlab gitlab/gitlab  --timeout 800s --set global.edition=ce --set global.hosts.domain=example.com --set global.ingress.enabled=false --set nginx-ingress.enabled=false --set certmanager-issuer.email=eliottdhommee@gmail.com --namespace=gitlab
 kubectl apply -f ../confs/gitlab-ingress.yaml -n gitlab
-kubectl apply -f ../confs/argo-app.yaml -n argocd
 IPEXT=`kubectl get ingress -n gitlab | grep gitlab | awk '{print $4}'`
 echo "$IPEXT gitlab.example.com" | sudo tee -a  /etc/hosts
-echo `kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode` > gitlab_password.txt
+echo `kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode` > gitlab-password.txt
--- p3/scripts/setup-cluster.sh
+++ p3/scripts/setup-cluster.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
-export K3D_FIX_DNS=1
+#apt-get update
+#apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#apt-cache policy docker-ce
+#apt-get install -y docker-ce
+#
+#wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+#
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 k3d cluster create part3 -p 8080:80@loadbalancer -p 8443:443@loadbalancer -p 4242:3006
 kubectl create namespace argocd
@@ -8,5 +18,4 @@ kubectl apply -f ../confs/install.yaml -n argocd
 kubectl apply -f ../confs/ingress.yaml -n argocd
 kubectl rollout status deployment argocd-server -n argocd
 kubectl apply -f ../confs/argo-app.yaml -n argocd
-echo `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d ` > argocd_password.txt
-echo `kubectl get ingress -n argocd | grep argocd | awk '{print $4}'` > argocd_ip.txt
+echo `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d ` > argocd-password.txt
